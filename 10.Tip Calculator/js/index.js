5d4c17d4b4460f287b1a10f0d5ab2e730149a2f3
vrokx <vrokx@users.noreply.github.com>
2021-01-30 19:23:15 +0530

    initial

--- 10.Tip Calculator/js/index.js
+++ 10.Tip Calculator/js/index.js
@@ -1,18 +1,8 @@
-/*var btn = document.getElementsByClassName("btn");
-
-function calculate(){
-    btn.classList.toggle("btnClicked");
-    setTimeout(removeClass , 400);
-
-    function removeClass(){
-        btn.classList.add("btn");
-    }
-}*/
 var amnt = document.getElementById("amnt");
 var people = document.getElementById("people");
 var service = document.getElementById("service");
 var para = document.querySelectorAll("p");
-
+var btn = document.querySelector(".btn");
 
 function calculate(){
     var tip = (amnt.value*service.value)/100 ;
@@ -23,4 +13,12 @@ function calculate(){
 
     var share = (total/parseInt(people.value));
     para[2].innerHTML = ("Each Person Owes: "  + share);
+
+    btn.style.background =  "#e0f7ff";
+    btn.style.boxShadow = "inset 5px 5px 10px #abced9, inset -5px -5px 10px #e7ffff";
+
+    setTimeout(() => {
+        btn.style.background =  "#c9f2ff";
+        btn.style.boxShadow = "5px 5px 10px #bed2d9,-5px -5px 10px #ffffff";
+    }, 150);
 }
